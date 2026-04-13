1756b10af8335b9d2c8d15cbfac60d8fb35c9aa3
Cameron-Nagel <cameronnagel8000@gmail.com>
2026-04-13 14:59:30 +0200

    add login & register validations

--- Mango.Web/DTOs/LoginRequestDto.cs
+++ Mango.Web/DTOs/LoginRequestDto.cs
@@ -1,8 +1,12 @@
-﻿namespace Mango.Web.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Web.DTOs
 {
     public class LoginRequestDto
     {
+        [Required]
         public string UserName { get; set; } = string.Empty;
+        [Required]
         public string Password { get; set; } = string.Empty;
 
     }
